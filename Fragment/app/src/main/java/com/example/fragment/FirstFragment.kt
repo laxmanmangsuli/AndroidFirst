52c9ff2f313c6947c25e4d3e49b496450156d787
Laxman IT <lbmangsuli79@gmail.com>
2023-07-04 21:54:31 +0530

    Add fragment1

---
+++ Fragment/app/src/main/java/com/example/fragment/FirstFragment.kt
@@ -0,0 +1,13 @@
+package com.example.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+}
---
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,firstFragment)
+            commit()
+        }
+        btnFragment1.s
+    }
+}
